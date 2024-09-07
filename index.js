460067af65c3cc01696e933bd4809f0bd588b9e9
asfya <abeyrami2005@gmail.com>
2024-09-07 15:58:19 +0330

    add history list item

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 // this is my first project of javascript , im ASFYA
 
-// enter the value
-function dataNumber(enterr){
-    document.getElementById('result').value += enterr ;
-}
+        // enter the value
+        function dataNumber(enterr){
+            document.getElementById('result').value += enterr;
+        }
 
-// delete the value
-function claerData(){
-    document.getElementById('result').value = " ";
-}
+        // delete the value
+        function claerData(){
+            document.getElementById('result').value = "";
+        }
 
-// result
-function sum() {  
-    const resultField = document.getElementById('result');  
-    try {  
-        resultField.value = eval(resultField.value);  
-    } catch (error) { 
-        resultField.value = 'error';  
-    }  
-}  
+        // نتیجه
+        function sum() {  
+            const resultField = document.getElementById('result'); // add result input to varable
+            const historyList = document.getElementById('history');// add history ui to varable
+             
+            const expression = resultField.value; // add sum in result to varable
+            const result = eval(expression); // sum the result
+            resultField.value = result;  // chenge result input to sum
+            
+            // Add to history
+            const historyItem = document.createElement('li');
+            historyItem.textContent = `${expression} = ${result}`;
+            historyList.insertBefore(historyItem, historyList.firstChild);  // show the sum prosses                           
+            
+        }  ;
+        
